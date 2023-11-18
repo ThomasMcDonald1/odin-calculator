9682e1a69a092daca0fceea07451a72bb74059f1
Thomas <osucollab@gmail.com>
2023-11-18 17:36:26 -0500

    adds Big.js implementations for performing math
    most operations implemented, although running into a bug where
    subtraction is being called

--- calculator.js
+++ calculator.js
@@ -1,23 +1,24 @@
 let buttons = document.querySelectorAll(".calc-button");
 let entryArea = document.querySelector("#entry-container");
 
-let currentOperator = "";
-let currentEntries = 0;
-let operatorApplied = false;
-const MAX_FIRST_OPS = 20;
-const MAX_SECOND_OPS = 20;
+//let currentOperator = "";
+//let currentEntries = 0;
+//let operatorApplied = false;
+//const MAX_FIRST_OPS = 20;
+// const MAX_SECOND_OPS = 20;
 const MAX_ENTRIES = 44;
 
 let firstNumber = 
 {
     operandVal: "",
-    hasDecimal: false
+    operatorUsed: "",
+    MAX_DIGITS: 20
 };
 
 let secondNumber = 
 {
     operandVal: "",
-    hasDecimal: false
+    MAX_DIGITS: 20
 };
 
 
@@ -31,27 +32,30 @@ buttons.forEach((button) =>
 
 function buttonClicked(button)
 {
+    if (firstNumber.operandVal === "ERROR") { resetAll(); }
     switch (button.textContent)
     {
         case "+/-":
-            //alert("+/- was pressed!");
-            compute("+/-");
+            if (firstNumber.operandVal.length > 0 && secondNumber.operandVal.length === 0 && firstNumber.operatorUsed === "")
+            {
+                Multiply(-1);
+            }
+            // Otherwise, we need to compute first, assign the result to the first number, then multiply by -1
+            else if (secondNumber.operandVal.length > 0) 
+            {
+                compute();
+                Multiply(-1);
+            }
             break;
-        case "AC":
-            //alert("AC was pressed!");
-            clearAll();
+        case "AC":          
+            resetAll();
             break;
         case "DEL":
-            //alert("DEL was pressed!");
+            removeLastInput();
             break;
         case "+":
-            //alert("+ was pressed!");
-            if (!operatorApplied)
-            {
-                currentOperator = "+";
-                displayOperator();
-            }
-            else if(secondNumber.operandVal.length > 0) { compute("+"); }
+            if (firstNumber.operandVal.length > 0) { checkOperator("+"); }
+            //else if(secondNumber.operandVal.length > 0) { compute("+"); }
             break;
         case "7":
             //alert("7 was pressed!");
@@ -66,13 +70,10 @@ function buttonClicked(button)
             checkNumber(9);
             break;
         case "-":
+            alert("why is this being clicked?!!");
             //alert("- was pressed!");
-            if (!operatorApplied)
-            {
-                currentOperator = "-";
-                displayOperator();
-            }
-            else if(secondNumber.operandVal.length > 0) { compute("-"); }
+            if (firstNumber.operandVal.length > 0) { checkOperator("-"); }
+            //else if(secondNumber.operandVal.length > 0) { compute("-"); }
             break;
         case "4":
             //alert("4 was pressed!");
@@ -88,12 +89,8 @@ function buttonClicked(button)
             break;
         case "x":
             //alert("x was pressed!");
-            if (!operatorApplied)
-            {
-                currentOperator = "*";
-                displayOperator();
-            }
-            else if(secondNumber.operandVal.length > 0) { compute("*"); }
+            if (firstNumber.operandVal.length > 0) { checkOperator("*"); }
+            //else if(secondNumber.operandVal.length > 0) { compute("*"); }
             break;
         case "1":
             //alert("1 was pressed!");
@@ -109,15 +106,25 @@ function buttonClicked(button)
             break;
         case "/":
             //alert("/ was pressed!");
-            if (!operatorApplied)
-            {
-                currentOperator = "/";
-                displayOperator();
-            }
-            else if(secondNumber.operandVal.length > 0) { compute("/"); }
+            if (firstNumber.operandVal.length > 0) { checkOperator("/"); }
+            //else if(secondNumber.operandVal.length > 0) { compute("/"); }
             break;
         case ".":
-            //alert(". was pressed!");
+            if (secondNumber.operandVal.length === 0 && firstNumber.operatorUsed === "" && 
+                firstNumber.operandVal.length > 0 && firstNumber.operandVal.length < firstNumber.MAX_DIGITS-1 &&
+                !firstNumber.hasDecimal)
+            {
+                firstNumber.hasDecimal = true;
+                firstNumber.operandVal += ".";
+                displayEntry(".");
+            }
+            else if (secondNumber.operandVal.length > 0 && secondNumber.operandVal.length < secondNumber.MAX_DIGITS-1 &&
+                     !secondNumber.hasDecimal)
+            {
+                secondNumber.hasDecimal = true;
+                secondNumber.operandVal += ".";
+                displayEntry(".");
+            }
             break;
         case "0":
             //alert("0 was pressed!");
@@ -126,7 +133,7 @@ function buttonClicked(button)
         case "=":
             //alert("= was pressed!");
             if (secondNumber.operandVal.length > 0) 
-                compute("=");
+                compute();
             break;               
         default:
             alert("ERROR... This shouldn't happen...");
@@ -136,6 +143,33 @@ function buttonClicked(button)
 
 function checkNumber(number)
 {
+    if (firstNumber.operatorUsed === "") // then we know the user clicked a number for the first number
+    {
+        if (firstNumber.operandVal.length < firstNumber.MAX_DIGITS && !(number === 0 && firstNumber.operandVal === "0"))
+        {
+            if (firstNumber.operandVal === "0" && number !== 0)
+            {
+                while(entryArea.firstChild) { entryArea.removeChild(entryArea.firstChild); }
+                firstNumber.operandVal = "";
+            }
+            firstNumber.operandVal += number;
+            displayEntry(number);
+        }
+    }
+    else
+    {
+        if (secondNumber.operandVal.length < secondNumber.MAX_DIGITS && !(number === 0 && secondNumber.operandVal === "0"))
+        {
+            if (secondNumber.operandVal === "0" && number !== 0) 
+            { 
+                while(entryArea.firstChild) { entryArea.removeChild(entryArea.firstChild); }
+                secondNumber.operandVal = ""; 
+            }
+            secondNumber.operandVal += number;
+            displayEntry(number);
+        }
+    }
+    /* OLD CODE
     if (!operatorApplied)
     {
         if (firstNumber.operandVal.length < MAX_FIRST_OPS && !(number === 0 && firstNumber.operandVal === "0"))
@@ -165,7 +199,7 @@ function checkNumber(number)
             currentEntries++;
             displayEntry(number);
         }
-    }
+    }*/
 }
 
 function displayEntry(entry)
@@ -176,8 +210,26 @@ function displayEntry(entry)
     entryArea.appendChild(displayEntry);
 }
 
-function displayOperator()
+function checkOperator(operator)
 {
+    /* Then we know the first number already contains an operator, so we can perform a calulation
+        before displaying the result and apply the next operator to said result */
+    if (secondNumber.operandVal.length > 0)
+    {
+        // Compute first, then show the result with this next operator:
+        compute();
+    }
+    /* We need this check to ensure we are not overriding a current operator already applied in the
+       case that the second number has not already been developed. If the previous if statement was
+       true where the second number WAS developed, then we know that the compute() function will reset
+       the firstNumber.operatorUsed = "" */
+    if (firstNumber.operatorUsed === "")
+    {
+        firstNumber.operatorUsed = operator;
+        displayEntry(operator);
+    }
+
+    /* OLD CODE
     if (firstNumber.operandVal.length > 0)
     {
         operatorApplied = true;
@@ -186,13 +238,19 @@ function displayOperator()
         displayEntry.textContent = currentOperator;
         entryArea.appendChild(displayEntry);
     }
+    */
 }
 
-function compute(operator)
+function compute()
 {
-    switch(operator)
+    let first = new Big(firstNumber.operandVal);
+    let second = new Big(secondNumber.operandVal);
+    switch(firstNumber.operatorUsed)
     {
-        case "+/-":
+        /*case "+/-":
+            // This checks to make sure only the first number is applied and doesn't have an operator
+            // Won't do anything if the firstNumber already has an operator
+             OLD CODE
             if (!operatorApplied) 
             { 
                 let tmp = BigInt(firstNumber.operandVal);
@@ -211,9 +269,18 @@ function compute(operator)
                 compute(currentOperator);
                 compute("+/-");
             }
-            break;
+            break;*/
         case "+":
             {
+                let third = first.plus(second);
+                firstNumber.operandVal = third.toString();
+                alert(firstNumber.operandVal);
+                clearDisplay();
+                for(let i = 0; i < firstNumber.operandVal.length; i++)
+                {
+                    displayEntry(firstNumber.operandVal[i]);
+                }
+                /* OLD CODE 
                 firstNumber.operandVal = solve().toString();
                 secondNumber.operandVal = "";
                 currentOperator = "+";
@@ -222,10 +289,19 @@ function compute(operator)
                 {
                     displayEntry(firstNumber.operandVal[i]);
                 }
-                displayOperator();
+                checkOperator();
                 break;
+                */
             }
         case "-":
+            alert("uhhh?");
+            firstNumber.operandVal = (first.minus(second)).toString();
+            clearDisplay();
+            for(let i = 0; i < firstNumber.operandVal.length; i++)
+            {
+                displayEntry(firstNumber.operandVal[i]);
+            }
+            /* OLD CODE
             firstNumber.operandVal = solve().toString();
             secondNumber.operandVal = "";
             currentOperator = "-";
@@ -234,9 +310,12 @@ function compute(operator)
             {
                 displayEntry(firstNumber.operandVal[i]);
             }
-            displayOperator();
+            checkOperator();
+            */
             break;
         case "*":
+            Multiply(secondNumber.operandVal);
+            /* OLD CODE
             firstNumber.operandVal = solve().toString();
             secondNumber.operandVal = "";
             currentOperator = "*";
@@ -245,9 +324,23 @@ function compute(operator)
             {
                 displayEntry(firstNumber.operandVal[i]);
             }
-            displayOperator();
+            checkOperator();
+            */
             break;
         case "/":
+            if (secondNumber.operandVal === "0") { displayError(); }
+            else
+            {
+                let first = new Big(firstNumber.operandVal);
+                firstNumber.operandVal = (first.div(new Big(secondNumber.operandVal))).toString();
+                clearDisplay();
+                for (let i = 0; i < firstNumber.operandVal.length; i++)
+                {
+                    displayEntry(firstNumber.operandVal[i]);
+                }
+            }
+
+            /* OLD CODE
             firstNumber.operandVal = solve().toString();
             if (firstNumber.operandVal === "ERROR") { displayError(); }
             secondNumber.operandVal = "";
@@ -257,17 +350,76 @@ function compute(operator)
             {
                 displayEntry(firstNumber.operandVal[i]);
             }
-            displayOperator();
+            checkOperator();
+            */
             break;
-        case "=":
-            break;
+        /*case "=":
+            if (secondNumber.operandVal.length > 0) { compute(); }
+            break;*/
         default:
             break;
     }
+    firstNumber.operatorUsed = "";
 }
 
-function solve()
+function Multiply(value)
 {
+    let first = new Big(firstNumber.operandVal);
+    firstNumber.operandVal = (first.times(new Big(value))).toString();
+    clearDisplay();
+    for (let i = 0; i < firstNumber.operandVal.length; i++)
+    {
+        displayEntry(firstNumber.operandVal[i]);
+    }
+}
+
+function clearDisplay()
+{
+    while (entryArea.firstChild)
+    {
+        entryArea.removeChild(entryArea.firstChild);
+    }
+    secondNumber.operandVal = "";
+}
+
+function resetAll()
+{
+    clearDisplay();
+    firstNumber.operandVal = "";
+    firstNumber.operatorUsed = "";
+}
+
+function removeLastInput()
+{
+    if (firstNumber.operatorUsed === "" && firstNumber.operandVal.length > 0)
+    {
+        firstNumber.operandVal = firstNumber.operandVal.slice(0, firstNumber.operandVal.length - 1);
+        entryArea.removeChild(entryArea.lastChild);
+    }
+    else if (firstNumber.operatorUsed !== "" && secondNumber.operandVal.length === 0)
+    {
+        firstNumber.operatorUsed = "";
+        entryArea.removeChild(entryArea.lastChild);
+    }
+    else if (secondNumber.operandVal.length > 0)
+    {
+        secondNumber.operandVal = secondNumber.operandVal.slice(0, secondNumber.operandVal.length - 1);
+        entryArea.removeChild(entryArea.lastChild);
+    }
+}
+
+function displayError()
+{
+    resetAll();
+    firstNumber.operandVal = "ERROR";
+    for (let i = 0; i < firstNumber.operandVal.length; i++)
+    {
+        displayEntry(firstNumber.operandVal[i]);
+    }
+}
+/*function solve()
+{
+    /* OLD CODE
     switch (currentOperator)
     {
         case "+":
@@ -284,22 +436,4 @@ function solve()
             return BigInt(firstNumber.operandVal) / BigInt(secondNumber.operandVal);
             break;
     }
-}
-
-/*
-let tmp = BigInt(secondNumber.operandVal);
-                tmp *= -1n;
-                if (tmp < 0) { currentEntries++; }
-                else { currentEntries--; }
-                secondNumber.operandVal = tmp.toString();     
-                while (entryArea.firstChild) { entryArea.removeChild(entryArea.firstChild); }
-                for (let i = 0; i < firstNumber.operandVal.length; i++)
-                {
-                    displayEntry(firstNumber.operandVal[i]);
-                }
-                displayOperator();
-                for(let i = 0; i < secondNumber.operandVal.length; i++)
-                {
-                    displayEntry(secondNumber.operandVal[i]);
-                }
-*/
+}*/
